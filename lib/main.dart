4956974488decc72c05b35ebc5068b9d465cf817
uchamod <uchamod52@gmail.com>
2024-05-24 13:49:21 +0530

    add new fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,8 +13,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Expenz manager",
       theme: ThemeData(
-          fontFamily: "Poppins",
-          canvasColor: const Color.fromARGB(255, 189, 145, 11)),
+        fontFamily: "Poppins",
+      ),
       home: const Scaffold(
         body: Center(
           child: Text(
